a81f2dff6fc44d54c86eacdda8b47ee4bf44d0e1
samah2022 <107212096+samah2022@users.noreply.github.com>
2023-03-14 09:59:36 +0100

    Create appAddTodoList.js

---
+++ appAddTodoList.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [todoList, setTodoList]= useState([]);
+  const [newTask, setNewTask]=useState("");
+
+  const handleChange= (event)=>{
+    setNewTask(event.target.value)
+  };
+  
+  const addTask= ()=>{
+    const newTodoList=[...todoList, newTask]
+    setTodoList(newTodoList);
+  }
+  return(
+    <div className="App">
+      "Do homework"
+      "Wash Dishes"
+      <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => {
+        return <h1>{task}</h1>;
+        })}
+      </div>
+    </div>
+);
+}
+
+export default App;
